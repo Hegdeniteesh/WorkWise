874696f35c8841e0e6121013bbb6059920f45845
Hegdeniteesh <niteeshhegde1721@gmail.com>
2025-09-09 15:48:50 +0530

    Add main Spring Boot application class

--- backend/src/main/java/com/workwise/WorkwiseApplication.java
+++ backend/src/main/java/com/workwise/WorkwiseApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkwiseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorkwiseApplication.class, args);
+		System.out.println(" WorkWise Backend Application Started Successfully!");
 	}
-
 }
