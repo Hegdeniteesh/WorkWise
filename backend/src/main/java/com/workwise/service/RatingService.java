38f8078ef99bf50e879ab4965694a273fc3224cc
Hegdeniteesh <niteeshhegde1721@gmail.com>
2025-09-17 16:37:15 +0530

    setup RatingService with dependencies and review

--- backend/src/main/java/com/workwise/service/RatingService.java
+++ backend/src/main/java/com/workwise/service/RatingService.java
@@ -1,4 +1,29 @@
 package com.workwise.service;
 
+import com.workwise.model.Rating;
+import com.workwise.repository.JobRepository;
+import com.workwise.repository.RatingRepository;
+import com.workwise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class RatingService {
+
+    @Autowired
+    private RatingRepository ratingRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public List<Rating> getUserReviews(Long userId) {
+        return ratingRepository.findByRatedUserId(userId);
+    }
 }
