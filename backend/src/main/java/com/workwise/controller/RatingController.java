4710afac9eaf29290f123b8ad55f504f3795f518
Hegdeniteesh <niteeshhegde1721@gmail.com>
2025-09-17 16:50:18 +0530

    setup RatingController with dependencies

--- backend/src/main/java/com/workwise/controller/RatingController.java
+++ backend/src/main/java/com/workwise/controller/RatingController.java
@@ -1,4 +1,25 @@
 package com.workwise.controller;
 
+import com.workwise.repository.JobRepository;
+import com.workwise.repository.UserRepository;
+import com.workwise.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ratings")
+@CrossOrigin(origins = "http://localhost:3000")
 public class RatingController {
+
+    @Autowired
+    private RatingService ratingService;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
 }
